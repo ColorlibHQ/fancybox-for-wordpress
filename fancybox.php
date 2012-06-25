13c1dc10c0e3d1eed2235d6346619353c1518fb9
José Pardilla <jose@moskis.net>
2012-06-25 09:07:19 +0200

    revert plugins_url() and use plugin_dir_url()

--- fancybox.php
+++ fancybox.php
@@ -51,8 +51,8 @@ $wp_plugin_dir = $wp_content_dir . '/plugins';
 
 */
 
-define( 'FBFW_PATH', plugins_url( 'fancybox-for-wordpress' ) . '/fancybox-for-wordpress' );
-define( 'FBFW_URL', plugins_url( 'fancybox-for-wordpress' ) . '/fancybox-for-wordpress' );
+define( 'FBFW_PATH', plugin_dir_path(__FILE__) );
+define( 'FBFW_URL', plugin_dir_url(__FILE__) );
 
 
 
@@ -62,7 +62,7 @@ define( 'FBFW_URL', plugins_url( 'fancybox-for-wordpress' ) . '/fancybox-for-wor
 
 define( 'FBFW_VERSION', '3.0.1' );
 
-require FBFW_PATH . '/settings.php';
+require FBFW_PATH . 'settings.php';
 
 
 
@@ -103,9 +103,9 @@ function mfbfw_register_scripts() {
 	}
 
 	// Register scripts
-	wp_register_script('fancybox', FBFW_URL . '/fancybox/jquery.fancybox.js', $jquery, '1.3.4', $footer ); // Main Fancybox script
-	wp_register_script('jqueryeasing', FBFW_URL . '/js/jquery.easing.1.3.min.js', false, '1.3', $footer ); // Easing animations script
-	wp_register_script('jquerymousewheel', FBFW_URL . '/js/jquery.mousewheel.3.0.4.pack.js', false, '3.0.4', $footer ); // Mouse wheel support script
+	wp_register_script('fancybox', FBFW_URL . 'fancybox/jquery.fancybox.js', $jquery, '1.3.4', $footer ); // Main Fancybox script
+	wp_register_script('jqueryeasing', FBFW_URL . 'js/jquery.easing.1.3.min.js', false, '1.3', $footer ); // Easing animations script
+	wp_register_script('jquerymousewheel', FBFW_URL . 'js/jquery.mousewheel.3.0.4.pack.js', false, '3.0.4', $footer ); // Mouse wheel support script
 
 
 }
@@ -141,7 +141,7 @@ add_action( 'wp_enqueue_scripts', 'mfbfw_scripts' ); // Load Scripts
 function mfbfw_styles() {
 
 	$settings = get_option( 'mfbfw' );
-	wp_enqueue_style( 'fancybox', FBFW_URL . '/fancybox/fancybox.css' );
+	wp_enqueue_style( 'fancybox', FBFW_URL . 'fancybox/fancybox.css' );
 
 	?>
 
@@ -271,7 +271,7 @@ add_action( 'wp_head', 'mfbfw_init' );
 function mfbfw_textdomain() {
 
 	if ( function_exists('load_plugin_textdomain') ) {
-		load_plugin_textdomain( 'mfbfw', FBFW_URL . '/languages', 'fancybox-for-wordpress/languages' );
+		load_plugin_textdomain( 'mfbfw', FBFW_URL . 'languages', 'fancybox-for-wordpress/languages' );
 	}
 
 }
@@ -321,7 +321,7 @@ add_action( 'admin_init', 'mfbfw_admin_options' );
 
 function mfbfw_admin_menu() {
 
-	require FBFW_PATH . '/admin.php';
+	require FBFW_PATH . 'admin.php';
 
 	$mfbfwadmin = add_submenu_page( 'options-general.php', 'Fancybox for WordPress Options', 'Fancybox for WP', 'manage_options', 'fancybox-for-wordpress', 'mfbfw_options_page' );
 
@@ -338,13 +338,13 @@ add_action('admin_menu', 'mfbfw_admin_menu');
  */
 
 function mfbfw_admin_styles() {
-	wp_enqueue_style( 'fancybox-admin', FBFW_URL . '/css/fancybox-admin.css' ); // Load custom CSS for Admin Page
-	wp_enqueue_style( 'jquery-ui', FBFW_URL . '/css/jquery-ui.css' ); // Load jQuery UI Tabs CSS for Admin Page
+	wp_enqueue_style( 'fancybox-admin', FBFW_URL . 'css/fancybox-admin.css' ); // Load custom CSS for Admin Page
+	wp_enqueue_style( 'jquery-ui', FBFW_URL . 'css/jquery-ui.css' ); // Load jQuery UI Tabs CSS for Admin Page
 }
 
 function mfbfw_admin_scripts() {
 	wp_enqueue_script( 'jquery-ui-tabs', array('jquery-ui-core') ); // Load jQuery UI Tabs JS for Admin Page
-	wp_enqueue_script( 'fancybox-admin', FBFW_URL . '/js/admin.js', array('jquery') ); // Load specific JS for Admin Page
+	wp_enqueue_script( 'fancybox-admin', FBFW_URL . 'js/admin.js', array('jquery') ); // Load specific JS for Admin Page
 }
 
 
