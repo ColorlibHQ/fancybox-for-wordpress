acb5d68647026f6504fcfa0269a255fea3300173
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-10-30 13:06:29 +0200

    update tested up to

--- fancybox.php
+++ fancybox.php
@@ -6,7 +6,7 @@
 * Version: 3.2.6
 * Author: Colorlib
 * Author URI: https://colorlib.com/wp/
-* Tested up to: 5.2
+* Tested up to: 5.3
 * Requires: 4.6 or higher
 * License: GPLv3 or later
 * License URI: http://www.gnu.org/licenses/gpl-3.0.html
