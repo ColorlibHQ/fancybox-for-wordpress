6d4008307c5f7f5a0c9cff01f362dfafff16c10e
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2020-04-27 10:53:43 +0300

    remove review request when clicking close
    remove review request when clicking close notice button and hide it untill next term

--- class-fancybox-review.php
+++ class-fancybox-review.php
@@ -145,6 +145,21 @@ class Fancybox_Review {
 
 				} );
 
+				$('#mfbfw-epsilon-review-notice .notice-dismiss').click(function(){
+
+					var data = {
+						action: 'epsilon_mfbfw_review',
+						security: '<?php echo $ajax_nonce; ?>',
+						check: 'epsilon-later'
+					};
+
+					$.post( '<?php echo admin_url( 'admin-ajax.php' ) ?>', data, function( response ) {
+						$( '#<?php echo $this->slug ?>-epsilon-review-notice' ).slideUp( 'fast', function() {
+							$( this ).remove();
+						} );
+					});
+				});
+
 			});
 		</script>
 
